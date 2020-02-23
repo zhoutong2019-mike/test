009aab72105da3700dee33e6c3a068c3602e6af8
929156096@qq.com <929156096@qq.com>
2020-02-23 20:36:05 +0800

    增加a.java文件

---
+++ a.java
@@ -0,0 +1,13 @@
+﻿public class Student{
+
+	private String name ;//版本1.0
+	private Character age ;//版本2.0
+	private Integer sId ;//版本3.0
+	private String className ;//版本3.1，新增字段，合并到4.0
+
+	public Student(){
+		super();
+	//版本4.0
+	}
+	
+}
